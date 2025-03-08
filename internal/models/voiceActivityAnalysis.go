38f54b2c828bfabda09687400c8def2c575948cf
Kosolpattanadurong Thitiwat <thitiwatipst@icloud.com>
2025-03-08 16:29:40 +0800

    fix db

--- internal/models/voiceActivityAnalysis.go
+++ internal/models/voiceActivityAnalysis.go
@@ -1,9 +1,10 @@
 package models
-
-
+import (
+	"time"
+	"gorm.io/gorm"
+)
 type VoiceActivityAnalysis struct {
 	ID   uint   `gorm:"primaryKey;autoIncrement" json:"id"`
-	DownloadURL string `gorm:"not null" json:"download_url"`
 	Duration float32 `gorm:"not null" json:"duration"`
 	SpeechDuration float32 `gorm:"not null" json:"speech_duration"`
 	PausesDuration float32 `gorm:"not null" json:"pauses_duration"`
@@ -11,4 +12,6 @@ type VoiceActivityAnalysis struct {
 	AnswerDelay float32 `gorm:"not null" json:"answer_delay"`
 	// Detailed number of pauses
 	Pauses []Pauses `json:"pauses"`
+    DeletedAt time.Time
+	IsDeleted gorm.DeletedAt `gorm:"softDelete:flag,DeletedAtField:DeletedAt" json:"deleted_at,omitempty"`
 }
